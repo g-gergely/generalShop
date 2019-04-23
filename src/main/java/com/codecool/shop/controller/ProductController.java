1a7866eb6a9cd643dbbb9186c15f3c5ccb12ed3d
Kiscapa <mashupmania22@gmail.com>
2019-04-23 23:10:00 +0200

    Generals can be filtered.

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -7,6 +7,8 @@ import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -16,29 +18,85 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         SupplierDao supplierDao = SupplierDaoMem.getInstance();
 
         Map params = new HashMap<String, Object>() {{
-            put("category", productCategoryDataStore.find(1));
+            put("categ", productCategoryDataStore.find(1));
             put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
             put("suppliers", supplierDao.getAll());
             put("categories", productCategoryDataStore.getAll());
         }};
 
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, resp, request.getServletContext());
         params.forEach(((key, value) -> context.setVariable(String.valueOf(key), value)));
-        engine.process("product/index.html", context, resp.getWriter());
+        engine.process("product/index", context, resp.getWriter());
     }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDao = SupplierDaoMem.getInstance();
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+
+        String category = request.getParameter("category");
+        String supplier = request.getParameter("supplier");
+
+        System.out.println(category);
+        System.out.println(supplier);
+
+        Map params = new HashMap<String, Object>() {{
+            put("suppliers", supplierDao.getAll());
+            put("categories", productCategoryDataStore.getAll());
+        }};
+        if (category == null && supplier == null) {
+            response.sendRedirect("/");
+        } else {
+            List<Product> products = new ArrayList<>();
+
+            if (category != null && supplier == null) {
+                products = productDataStore.getAll().stream()
+                        .filter(product -> product.getProductCategory().getName().equals(category))
+                        .collect(Collectors.toList());
+
+            } else if (category == null && supplier != null) {
+                products = productDataStore.getAll().stream()
+                        .filter(product -> product.getSupplier().getName().equals(supplier))
+                        .collect(Collectors.toList());
+
+            } else if (category != null && supplier != null) {
+                products = productDataStore.getAll().stream()
+                        .filter(product -> product.getProductCategory().getName().equals(category))
+                        .filter(product -> product.getSupplier().getName().equals(supplier))
+                        .collect(Collectors.toList());
+            }
+
+            if (products.size() > 0) {
+                ProductCategory newCateg = products.stream().map(Product::getProductCategory).findFirst().orElse(null);
+
+                params.put("products", products);
+                params.put("categ", newCateg);
+            }
+
+            params.forEach(((key, value) -> context.setVariable(String.valueOf(key), value)));
+            engine.process("product/index", context, response.getWriter());
+
+        }
+    }
 }
