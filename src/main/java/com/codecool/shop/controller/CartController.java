da7f8f9b561fca0f0d09cf91affd025528279355
Brigitta Arvay <arvay.brigitta22@gmail.com>
2019-04-24 13:57:42 +0200

    Modification on items in cart working.

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -11,16 +11,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet(urlPatterns = {"/cart"})
 public class CartController extends HttpServlet {
+    private Map<String, Integer> cart = ProductController.getCartMap();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
 
-        context.setVariable("cartMap", ProductController.getCartMap());
+
+        String addKey = request.getParameter("add");
+        String removeKey = request.getParameter("remove");
+
+        if (addKey != null) {
+            int value = cart.get(addKey);
+            cart.put(addKey, value+1);
+        }
+
+        if (removeKey != null) {
+            int value = cart.get(removeKey);
+            if (value < 2) {
+                cart.remove(removeKey);
+            } else {
+                cart.put(removeKey, value - 1);
+            }
+        }
+
+        context.setVariable("cartMap", cart);
         engine.process("product/cart", context, response.getWriter());
 
 
