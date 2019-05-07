14b834cc72e6b1b00449dc085678755ebe7f6936
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-05-07 14:06:02 +0200

    store.sql file added

---
+++ src/main/webapp/sql/store.sql
@@ -0,0 +1,25 @@
+create table supplier
+(
+    id serial not null primary key,
+    name varchar not null,
+    description varchar not null
+);
+
+create table product_category
+(
+    id serial not null primary key,
+    name varchar not null,
+    department varchar not null,
+    description varchar not null
+);
+
+create table product
+(
+    id serial not null primary key,
+    name varchar not null,
+    default_price numeric(12,2) not null,
+    currency varchar not null,
+    description varchar not null,
+    supplier integer references supplier(id),
+    product_category integer not null references product_category(id)
+);
