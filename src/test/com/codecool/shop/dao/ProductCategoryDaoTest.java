4d4ad2ecbbeaf96b10b338563dab8a52cfe63dd3
gaborBKI <gabor.bki@gmail.com>
2019-05-08 13:06:54 +0200

    Started working on parameterized tests.

---
+++ src/test/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -0,0 +1,42 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.dao.implementation.ProductCategoryDaoDb;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.model.ProductCategory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductCategoryDaoTest {
+
+    private ProductCategory test;
+
+    private static Stream<ProductCategoryDao> getClasses() {
+        return Stream.of(
+                ProductCategoryDaoMem.getInstance(),
+                ProductCategoryDaoDb.getInstance());
+    }
+
+    @BeforeEach
+    private void init(){
+        test = new ProductCategory(400, "Roman2", "Lieutenant", "Let's go");
+    }
+
+    @ParameterizedTest
+    @MethodSource("getClasses")
+    public void testAdd(ProductCategoryDao category){
+        category.add(test);
+        assertEquals(category.find(400), test);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getClasses")
+    public void testRemove(ProductCategoryDao category){
+        category.remove(100);
+    }
+}
