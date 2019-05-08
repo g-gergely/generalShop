9805b850672c8233df75961a9ea42fe060c9d44c
Peto Reka <peto.reka0@gmail.com>
2019-05-08 19:45:49 +0200

    ProductDaoTest still not working, but added the tests.

--- src/test/com/codecool/shop/dao/ProductDaoTest.java
+++ src/test/com/codecool/shop/dao/ProductDaoTest.java
@@ -7,19 +7,24 @@ import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ProductDaoTest {
 
     private Product test;
+    private Supplier testSupplier;
+    private Supplier falseSupplier;
+    private ProductCategory testCategory;
+    private ProductCategory falseCategory;
 
-    private static Stream<ProductDao> getClasses() {
+    private static Stream<ProductDao> getProductClasses() {
         return Stream.of(
                 ProductDaoMem.getInstance(),
                 ProductDaoDb.getInstance());
@@ -27,15 +32,96 @@ public class ProductDaoTest {
 
     @BeforeAll
     private void init() {
-        ProductCategory japanese = new ProductCategory(5, "Japanese", "General", "Tenka Fubu.");
-        Supplier owari = new Supplier(9, "Owari", "The start of something big");
-        test = new Product(1000, "Oda Nobunaga", 1700, "TAL", "The first of the 3 unifiers of Japan, lord of Owari.", japanese, owari);
+        testCategory = new ProductCategory(400, "RomanCategory", "Lieutenant", "Let's go");
+        falseCategory = new ProductCategory(401, "FalseCategory", "Lieutenant", "Let's go");
+        testSupplier = new Supplier(500, "RomanSupplier", "Colonel");
+        falseSupplier = new Supplier(501, "FalseSupplier", "FalseColonel");
+        test = new Product(100, "Oda Nobunaga", 1700, "TAL", "The first of the 3 unifiers of Japan, lord of Owari.", testCategory, testSupplier);
     }
 
+    @Order(1)
     @ParameterizedTest
-    @MethodSource("getClasses")
-    public void testAdd(ProductDao product){
+    @MethodSource("getProductClasses")
+    public void testProductAdd(ProductDao product){
         product.add(test);
-        assertNotNull(product.find(1000));
+        assertNotNull(product.find(100));
+    }
+
+    @Order(2)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductFindByID(ProductDao product){
+        product.add(test);                                                    //If i don't add again, it fails the ProductDaoMem Test!
+        assertEquals("Oda Nobunaga", product.find(100).getName());
+    }
+
+    @Order(3)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductGetAllProduct(ProductDao product){
+        assertEquals(1, product.getAll().size());
+    }
+
+    @Order(4)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductFindBySupplier(ProductDao product){
+        assertEquals(1, product.getBy(testSupplier).size());
+    }
+
+    @Order(5)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductFindByFalseSupplier(ProductDao product){
+        assertEquals(0, product.getBy(falseSupplier).size());
+    }
+
+    @Order(6)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductFindByProductCategory(ProductDao product){
+        assertEquals(1, product.getBy(testCategory).size());
+    }
+
+    @Order(7)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductFindByFalseProductCategory(ProductDao product){
+        assertEquals(0, product.getBy(falseCategory).size());
+    }
+
+    @Order(8)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductGetProductsByString(ProductDao product){
+        assertEquals(1, product.getProducts("RomanCategory","RomanSupplier").size());    //if i take out the second add it is okay
+    }
+    @Order(9)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductGetProductsByFalseString(ProductDao product){
+        assertEquals(0, product.getProducts("RomanvfvfvfCategory","RomanvfvfvfSupplier").size());
+    }
+
+    @Order(10)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductGetProductByObject(ProductDao product){
+        assertEquals(1, product.getProducts(testSupplier,testCategory).size());
+    }
+
+    @Order(11)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductGetProductByFalseObject(ProductDao product){
+        assertEquals(0, product.getProducts(falseSupplier,falseCategory).size());
+    }
+
+    @Order(12)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductRemove(ProductDao product){
+        product.remove(400);
+        assertNull(product.find(400));
     }
 }
