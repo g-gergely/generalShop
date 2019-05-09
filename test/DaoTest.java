99cefc84920a6d5cbfc0a6924a1c656bc5ed07b4
gaborBKI <gabor.bki@gmail.com>
2019-05-09 11:11:31 +0200

    Implemented all tests. Errors: ProductDaoDb -> if we search for a product with an empty string it returns all, in line 135 there is a NullPointerException thrown when searching for a non-existent supplier or category.

--- test/DaoTest.java
+++ test/DaoTest.java
@@ -1,10 +1,9 @@
 import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoDb;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.*;
 
-import com.codecool.shop.dao.implementation.SupplierDaoDb;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -21,7 +20,10 @@ class DaoTest {
     //these will be initialized before the tests run
     private ProductCategory testCategory;
     private Supplier testSupplier;
+    private Product testProduct;
 
+    private Supplier falseSupplier;
+    private ProductCategory falseCategory;
 
     //create a stream that holds an instance of every class that implements ProductCategoryDao
     private static Stream<ProductCategoryDao> getCategoryClasses() {
@@ -37,11 +39,21 @@ class DaoTest {
                 SupplierDaoDb.getInstance());
     }
 
+    private static Stream<ProductDao> getProductClasses() {
+        return Stream.of(
+                ProductDaoMem.getInstance(),
+                ProductDaoDb.getInstance());
+    }
+
     //initialize a ProductCategory and a Supplier object with these attributes:
     @BeforeAll
     public void setUp(){
         testCategory = new ProductCategory(400, "RomanCategory", "Lieutenant", "Let's go");
         testSupplier = new Supplier(500, "RomanSupplier", "Colonel");
+        testProduct = new Product(100, "RomanMan", 1700, "TAL", "A man", testCategory, testSupplier);
+
+        falseCategory = new ProductCategory(401, "FalseCategory", "FalseLieutenant", "Let's go");
+        falseSupplier = new Supplier(501, "FalseSupplier", "FalseColonel");
     }
 
     // The following tests will run multiple times each.
@@ -66,34 +78,91 @@ class DaoTest {
 
     @Order(3)
     @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductAdd(ProductDao product){
+        product.add(testProduct);
+        assertNotNull(product.find(100));
+    }
+
+    @Order(4)
+    @ParameterizedTest
     @MethodSource("getCategoryClasses")
     public void testCategoryFind(ProductCategoryDao category){
         assertEquals("RomanCategory", category.find(400).getName());
     }
 
-    @Order(4)
+    @Order(5)
     @ParameterizedTest
     @MethodSource("getSupplierClasses")
     public void testSupplierFind(SupplierDao supplier){
         assertEquals("RomanSupplier", supplier.find(500).getName());
     }
 
-    @Order(5)
+    @Order(6)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductFindByID(ProductDao product){
+        assertEquals("RomanMan", product.find(100).getName());
+    }
+
+    @Order(7)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductFindBySupplier(ProductDao product){
+        assertEquals(1, product.getBy(testSupplier).size());
+        assertEquals(0, product.getBy(falseSupplier).size());
+    }
+
+    @Order(8)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductFindByProductCategory(ProductDao product){
+        assertEquals(1, product.getBy(testCategory).size());
+        assertEquals(0, product.getBy(falseCategory).size());
+    }
+
+    @Order(9)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testGetProductsByString(ProductDao product){
+        assertEquals(1, product.getProducts(testCategory.getName(),testSupplier.getName()).size());
+        assertEquals(0, product.getProducts("jhin","jinihbn").size());
+        assertEquals(0, product.getProducts(testCategory.getName(), "gubgbg").size());
+        assertEquals(0, product.getProducts("ubzb", testSupplier.getName()).size());
+    }
+
+    @Order(10)
     @ParameterizedTest
     @MethodSource("getCategoryClasses")
     public void testGetAllCategories(ProductCategoryDao category){
         assertEquals(1, category.getAll().size());
     }
 
-    @Order(6)
+    @Order(11)
     @ParameterizedTest
     @MethodSource("getSupplierClasses")
     public void testGetAllSuppliers(SupplierDao supplier){
         assertEquals(1, supplier.getAll().size());
     }
 
+    @Order(12)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductGetAllProducts(ProductDao product){
+        assertEquals(1, product.getAll().size());
+    }
+
     //remove is left for last as it will remove the data from the database as well
-    @Order(7)
+    @Order(13)
+    @ParameterizedTest
+    @MethodSource("getProductClasses")
+    public void testProductRemove(ProductDao product){
+        product.remove(100);
+        assertNull(product.find(100));
+        assertEquals(0, product.getAll().size());
+    }
+
+    @Order(14)
     @ParameterizedTest
     @MethodSource("getCategoryClasses")
     public void testCategoryRemove(ProductCategoryDao category){
@@ -103,7 +172,7 @@ class DaoTest {
     }
 
 
-    @Order(8)
+    @Order(15)
     @ParameterizedTest
     @MethodSource("getSupplierClasses")
     public void testSupplierRemove(SupplierDao supplier){
