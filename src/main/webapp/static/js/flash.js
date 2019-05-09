8f84f1ebb577a209cec2743cdd62b8941e027334
gaborBKI <gabor.bki@gmail.com>
2019-05-09 16:05:30 +0200

    Created a page where Reka throws a flashbang.

---
+++ src/main/webapp/static/js/flash.js
@@ -0,0 +1,9 @@
+function backroundChanger(){
+    setTimeout(function()
+    { document.getElementsByTagName("BODY")[0].style.backgroundImage =
+        "url(https://wallpaperplay.com/walls/full/7/e/e/102458.jpg)";
+        alert("Throwing a Flashbang!")
+    }, 1000);
+}
+
+backroundChanger();
